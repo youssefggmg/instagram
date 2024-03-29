8beadde02cdd308b6846e8cbe0ca63529aa498d6
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-03-29 14:28:07 +0000

    ingram

---
+++ js/js.js
@@ -0,0 +1,28 @@
+// to add like to photho 1
+
+let btn=document.getElementById("pho1");
+let addlike=document.getElementById("like1");
+btn.onclick = function (){
+    addlike.classList.toggle("tolike");
+}
+// console.log(addlike.classList);
+// console.log( typeof addlike.classList);
+
+// to add like to photho 2
+let btn2=document.getElementById("pho2");
+let addlike2=document.getElementById("like2");
+btn2.onclick = function (){
+    addlike2.classList.toggle("tolike");
+}
+// to add like to photho 3
+let btn3=document.getElementById("pho3");
+let addlike3=document.getElementById("like3");
+btn3.onclick = function (){
+    addlike3.classList.toggle("tolike");
+}
+// to add like to photho 4
+let btn4=document.getElementById("pho4");
+let addlike4=document.getElementById("like4");
+btn4.onclick = function (){
+    addlike4.classList.toggle("tolike");
+}
